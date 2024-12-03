8c3e505cd0e8e216f72d40b41f2a61855525fe97
DawsonZorn <dawsonzorn@outlook.com>
2024-12-02 18:01:21 -0600

    Added a search bar with a customized feature i found online and updated the home controller to allow search functions for the items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,15 @@ class HomeController < ApplicationController
     # Apply filters
     @products = @products.on_sale if params[:filter] == "on_sale"
     @products = @products.new_products if params[:filter] == "new"
+
+    if params[:search].present? || params[:category_id].present?
+      @products = @products.joins(:category).where("products.name LIKE :search OR products.description LIKE :search", search: "%#{params[:search]}%") if params[:search].present?
+
+      if params[:category_id].present?
+        @products = @products.where(category_id: params[:category_id])
+      end
+    else
+      @products = []
+    end
   end
 end
